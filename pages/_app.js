03345a9732294312e31c44a0f7f09fff9f328c4f
AbdulManan Ali <abdulmananali46@gmail.com>
2023-11-25 23:49:38 +0500

    changing in files

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,35 @@
-// import '@/styles/globals.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/offcanvas-navbar.css";
-// import '../styles/global.css';
-import "../styles/modals.css";
-import "../styles/stakebutton.css";
-import "sf-font";
-import { useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/offcanvas-navbar.css'
+import '../styles/globals.css'
+import '../styles/modals.css'
+import '../styles/stakebutton.css'
+import 'sf-font';
+import { connectWallet } from '../components/config';
+import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  // const [status, getStatus] = useState('Connect Wallet');
+  const [status, getStatus] = useState('Connect Wallet');
+  
+  async function connect() {
+    let walletaddress = await connectWallet().then((output) => {
+      return output.connection.account.address;
+    });
+    localStorage.setItem('wallet', walletaddress);
+  }
 
-  // async function connect() {
-  //   let walletaddress = await connectWallet().then((output) => {
-  //     return output.connection.account.address
-  //   });
-  //   localStorage.setItem('wallet', walletaddress);
-  // }
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     let walletid = localStorage.getItem('wallet');
-  //     if (walletid != null) {
-  //       getStatus('Connected');
-  //     }
-  //   }, 4000)
-  // }, []);
-
+  useEffect(() => {
+    setInterval(() => {
+      let walletid = localStorage.getItem('wallet');
+      if (walletid != null) {
+        getStatus('Connected');
+      }
+    }, 4000)
+  }, []);
+  
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
-
+  
   return (
     <div>
 
@@ -42,7 +42,7 @@ function MyApp({ Component, pageProps }) {
             <ul className="nav col-12 col-lg me-lg mb-2 justify-content-left mb-md-0" style={{ fontSize: '20px', fontWeight: "bold" }}>
               <li><a href="#" className="nav-link px-5 text-white">Stake</a></li>
             </ul>
-            {/* <button className='btn btn-md btn-primary' onClick={connect}>{status}</button> */}
+            <button className='btn btn-md btn-primary' onClick={connect}>{status}</button>
           </div>
         </div>
       </header>
